e5e7d2691f0044484057cbf1f552018cd53d01d4
Gerald <ace4c3s@gmail.com>
2025-04-02 15:53:48 +0300

    palindrome number check

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+# This program checks if a number is a palindrome
+
+def isPalindrome(num):
+    if num < 0:
+        return False
+    
+    num_duplicate = num
+    reversed = 0
+
+    while num_duplicate > 0:
+        last_digit = num_duplicate%10
+        reversed = reversed * 10 + last_digit
+        num_duplicate //= 10
+    
+    if num == reversed:
+        return True
+    else:
+        return False
+    
+print(isPalindrome(104))
